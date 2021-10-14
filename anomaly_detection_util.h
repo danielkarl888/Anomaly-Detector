9098677ba557c93070f7ed04f05f98aa69108e85
Daniel Karl <danielkarl888@gmail.com>
2021-10-14 16:25:46 +0300

    1st original header file from Moodle.

---
+++ anomaly_detection_util.h
@@ -0,0 +1,31 @@
+
+#ifndef PROJECT2NDYEAR_ANOMALY_DETECTION_UTIL_H
+#define PROJECT2NDYEAR_ANOMALY_DETECTION_UTIL_H
+#endif //PROJECT2NDYEAR_ANOMALY_DETECTION_UTIL_H
+
+// returns the variance of X and Y
+float var(float* x, int size);
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size);
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size);
+class Line{
+public:
+    float a,b;
+    Line():a(0),b(0){}
+    Line(float a, float b):a(a),b(b){}
+    float f(float x){
+        return a*x+b;
+    }
+};
+class Point{
+public:
+    float x,y;
+    Point(float x, float y):x(x),y(x){}
+};
+// performs a linear regression and return s the line equation
+Line linear_reg(Point** points, int size);
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size);
+// returns the deviation between point p and the line
+float dev(Point p,Line l);
