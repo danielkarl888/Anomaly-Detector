fb1c5492480044263651571964ee8544ddde3e18
Daniel Karl <danielkarl888@gmail.com>
2021-10-14 23:04:07 +0300

    fix the main.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    int arr[5] = {10,2,3,4,10};
-    *arr = 100;
-    std::cout << avg(reinterpret_cast<float *>(arr), 5);
-
+    float arr[5] = {10,2,3,4,10};
+    std::cout << var(arr, 5);
     return 0;
 }
