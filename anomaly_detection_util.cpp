97f771588bcb39d294151a9ccb676f981ad15bfe
Daniel Karl <danielkarl888@gmail.com>
2021-10-14 16:59:42 +0300

    method avg added.

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,8 +1,18 @@
-//
-// Created by dvir landau on 14/10/2021.
-//
 // returns the variance of X and Y
 float var(float* x, int size){
 
+}
+/**
+ * return the average of the array.
+ * @param x is an array of numbers
+ * @param size of the array.
+ * @return the average.
+ */
+float avg(float* x, int size) {
+    float sum;
+    for (i = 0 ; i < size; i++) {
+        sum += x[i];
+    }
+    return sum/size;
 }
 
