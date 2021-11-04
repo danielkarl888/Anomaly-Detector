6add3b47574cfc994d76a7f0f96e0bd0c054dc68
Daniel Karl <danielkarl888@gmail.com>
2021-11-04 22:42:39 +0200

    check of creating a new branch in ex2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 
 #include <iostream>
 #include "anomaly_detection_util.h"
-
+#include "map"
+#include "vector"
+#include "string"
 using namespace std;
 
 bool wrong(float val, float expected){
@@ -10,29 +12,14 @@ bool wrong(float val, float expected){
 
 // this is a simple test to put you on the right track
 int main(){
-    const int N=10;
-    float x[]={1,2,3,4,5,6,7,8,9,10};
-    float y[]={2.1,4.2,6.1,8.1,10.3,12.2,14.4,16.1,18.2,20.3};
-
-    Point* ps[N];
-    for(int i=0;i<N;i++)
-        ps[i]=new Point(x[i],y[i]);
-
-    Line l=linear_reg(ps,N);
-    Point p(4,8);
-
-    float v[]={var(x,N),cov(x,y,N),pearson(x,y,N),l.a,l.b,l.f(4),dev(p,l)};
-    float e[]={8.25,16.63,0.999,2.015,0.113,8.176,0.176};
-
-
-    for(int i=0;i<7;i++)
-        if(wrong(v[i],e[i]))
-            cout<<"error for check "<<i<<" (-14)"<<endl;
-
-
-    for(int i=0;i<N;i++)
-        delete ps[i];
-
-    cout<<"done"<<endl;
+    map<string, vector<float>> a;
+    vector<float> b;
+    b.push_back(1.2);
+    b.push_back(2.3);
+    vector<float> c;
+    c.push_back(8.2);
+    c.push_back(9.3);
+    a.insert({"a", b});
+    a.insert({"b", c});
     return 0;
 }
--- timeseries.h
+++ timeseries.h
@@ -1,16 +1,22 @@
 
-
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
-
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <fstream>
 using namespace std;
 
 class TimeSeries{
 
 public:
+    map<string, vector<float>> map;
+	TimeSeries(const char* CSVfileName) {
+        ifstream myFile(CSVfileName);
+        myFile.good()
 
-	TimeSeries(const char* CSVfileName){
-	}
+
+    }
 
 };
 
