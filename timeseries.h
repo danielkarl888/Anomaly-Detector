0519c9f9b3870ae0d4400422cf5cb6854f6ca51b
Daniel Karl <danielkarl888@gmail.com>
2021-11-05 10:19:28 +0200

    first version of ctor of Timeseries

--- timeseries.h
+++ timeseries.h
@@ -5,15 +5,56 @@
 #include <vector>
 #include <map>
 #include <fstream>
+#include <string>
 using namespace std;
 
 class TimeSeries{
 
 public:
-    map<string, vector<float>> map;
+    map<string, vector<float>> table;
 	TimeSeries(const char* CSVfileName) {
         ifstream myFile(CSVfileName);
-        myFile.good()
+        if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+        string line, feature;
+        int val;
+        if(myFile.good())
+        {
+            // Extract the first line in the file
+            std::getline(myFile, line);
+
+            // Create a stringstream from line
+            std::stringstream ss(line);
+
+            // Extract each column name
+            while(std::getline(ss, feature, ',')){
+
+                // Initialize and add <feature, float> pairs to the table map
+                table.push_back({feature, std::vector<float> {}});
+            }
+        }
+        // Read data, line by line
+        while(std::getline(myFile, line))
+        {
+            // Create a stringstream of the current line
+            std::stringstream ss(line);
+
+            // Keep track of the current column index
+            int colIdx = 0;
+
+            // Extract each float
+            while(ss >> val){
+
+                // Add the current float to the 'colIdx' column's values vector
+                table.at(colIdx).second.push_back(val);
+
+                // If the next token is a comma, ignore it and move on
+                if(ss.peek() == ',') ss.ignore();
+
+                // Increment the column index
+                colIdx++;
+            }
+        }
+
 
 
     }
