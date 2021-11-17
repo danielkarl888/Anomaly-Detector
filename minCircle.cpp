ff4703584a3859176d6be1be5c2d7e7a273caa2f
Daniel Karl <danielkarl888@gmail.com>
2021-11-17 23:31:58 +0200

    added ex3 files

---
+++ minCircle.cpp
@@ -0,0 +1,1 @@
+#include "minCircle.h"
---
+++ minCircle.h
@@ -0,0 +1,28 @@
+
+#ifndef MINCIRCLE_H_
+#define MINCIRCLE_H_
+
+#include <iostream>
+#include <vector>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include "anomaly_detection_util.h"
+
+using namespace std;
+
+
+// ------------ DO NOT CHANGE -----------
+
+class Circle{
+public:
+	Point center;
+	float radius;
+	Circle(Point c,float r):center(c),radius(r){}
+};
+// --------------------------------------
+
+Circle findMinCircle(Point** points,size_t size);
+
+// you can add here additional methods
+
+#endif /* MINCIRCLE_H_ */
