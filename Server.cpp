4a113090d188849dde9163ae0ed81ebe682048d6
Daniel Karl <danielkarl888@gmail.com>
2021-12-29 23:44:59 +0200

    ex6 update after using linux

--- Server.cpp
+++ Server.cpp
@@ -1,17 +1,72 @@
 
 #include "Server.h"
-
+/**
+ * setting of the server - the file descriptor of the server.
+ * making the server ready for the start method that handles the client.
+ * @param port
+ */
 Server::Server(int port)throw (const char*) {
-
+    fdServer = socket(AF_INET, SOCK_STREAM, 0);
+    if (fdServer < 0 ) {
+        throw "socket failure";
+    }
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+    if (bind (fdServer, (struct sockaddr*) &server, sizeof (server)) < 0) {
+        throw "bind failure";
+    }
+    if(listen(fdServer, 2) < 0) {
+        throw "listen failure";
+    }
 }
-
-void Server::start(ClientHandler& ch)throw(const char*){	
+// using strategy pattern
+void Server::start(ClientHandler& ch)throw(const char*){
+    t = new thread([this, &ch](){
+        while (!stopping){
+            socklen_t size = sizeof(client);
+            int fdClient = accept(fdServer, (struct sockaddr*)&client, &size);
+            if(fdClient>0){
+                ch.handle(fdClient);
+                close(fdClient);
+            }
+        }
+        close(fdServer);
+    });
 }
 
 void Server::stop(){
-	t->join(); // do not delete this!
+    stopping = true;
+    t->join(); // do not delete this!
 }
 
 Server::~Server() {
+}
+
+string socketIO::read(){
+    char x = 0;
+    string str="";
+    // get every char in the string into x
+    while(x != '\n'){
+        recv(clientID,&x,sizeof(char),0);
+        str += x;
+    }
+    return str;
+}
+void socketIO::write(string text){
+    const char* txt=text.c_str();
+    send(clientID,txt,strlen(txt),0);
+}
+
+void socketIO::write(float f){
+    ostringstream ss;
+    ss <<f;
+    string s(ss.str());
+    write(s);
+}
+
+void socketIO::read(float* f){
+    //recv(clientID,f,sizeof(float),0);
+    // it will be already in the string line
 }
 
--- Server.h
+++ Server.h
@@ -3,39 +3,69 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+#include <iostream>
+#include <sys/socket.h>  // The header file socket.h includes a number of definitions of structures needed for sockets.
+#include <netinet/in.h>  // The header file in.h contains constants and structures needed for internet domain addresses.
+#include <pthread.h>
+#include <thread>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sstream>
+#include "CLI.h"
 
 using namespace std;
 
 // edit your ClientHandler interface here:
 class ClientHandler{
-    public:
+public:
     virtual void handle(int clientID)=0;
 };
 
 
 // you can add helper classes here and implement on the cpp file
+class socketIO:public DefaultIO{
+    int clientID;
+public:
 
+    socketIO(int clientID):clientID(clientID){}
+
+    virtual string read();
+    virtual void write(string text);
+
+    virtual void write(float f);
+
+    virtual void read(float* f);
+
+};
 
 // edit your AnomalyDetectionHandler class here
 class AnomalyDetectionHandler:public ClientHandler{
-	public:
+public:
     virtual void handle(int clientID){
-
+        socketIO socIO(clientID);
+        CLI cli(&socIO);
+        cli.start();
     }
 };
 
 
 // implement on Server.cpp
 class Server {
-	thread* t; // the thread to run the start() method in
+    int fdServer;
+    sockaddr_in server;
+    sockaddr_in client;
+    thread* t; // the thread to run the start() method in
+    volatile bool stopping = false;
 
-	// you may add data members
+    // you may add data members
 
 public:
-	Server(int port) throw (const char*);
-	virtual ~Server();
-	void start(ClientHandler& ch)throw(const char*);
-	void stop();
+    Server(int port) throw (const char*);
+    virtual ~Server();
+    void start(ClientHandler& ch)throw(const char*);
+    void stop();
 };
 
 #endif /* SERVER_H_ */
