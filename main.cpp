99843e4553a32db1a728aae1b8758f6f58bad69d
Daniel Karl <74876857+danielkarl888@users.noreply.github.com>
2024-05-30 09:46:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,46 +15,6 @@ bool wrong(float val, float expected){
 // this is a simple test to put you on the right track
 int main(){
     TimeSeries ts("C:\\Users\\Daniel Karl\\Desktop\\test.csv");
-//    vector<float> ttt = ts.getOneFeatureData("b");
-//    vector<string> featNames=ts.getNameFeatures();
-//    float vals[ts.getNumOfFeatures()][ts.getNumOfRows()];
-//    for(int i=0;i<featNames.size();i++) {
-//        for (size_t j = 0; j < ts.getNumOfFeatures(); j++) {
-//            vals[i][j] = ts.getOneFeatureData(featNames[i])[j];
-//        }
-//    }
-//    int numOfFeatures = ts.getNumOfFeatures();
-//    int numOfRecords = ts.getNumOfRecords();
-//    vector<string> featNames=ts.getNameFeatures();
-//    float vals[numOfFeatures][numOfRecords];
-//    for(int i=0;i<numOfFeatures;i++) {
-//        for (int j = 0; j < numOfRecords; j++) {
-//            vals[i][j] = ts.getFeatureDateAtTime(featNames[i], j + 1);
-//        }
-//    }
-//    for (int i = 0; i < numOfFeatures; ++i) {
-//        float maxPerson = 0;
-//        float currentPearson;
-//        int correlateIndex = -1;
-//        for (int j = i+1; j < numOfFeatures ; ++j) {
-//            currentPearson = abs(pearson(vals[i], vals[j], numOfRecords));
-//            if (currentPearson > maxPerson) {
-//                maxPerson = currentPearson;
-//                correlateIndex = j;
-//            }
-//        }
-//        if (correlateIndex != -1) {
-//            correlatedFeatures newCorelated;
-//            newCorelated.corrlation = maxPerson;
-//            newCorelated.feature1 = featNames[i];
-//            newCorelated.feature2 = featNames[correlateIndex];
-//            //newCorelated.lin_reg = Line()
-//
-//        }
-//    }
-    //float y = pearson(vals[0], vals[1],ts.getNumOfRows());
-//    correlatedFeatures x;
-//    x.corrlation = 5;
 
     SimpleAnomalyDetector sh;
     sh.learnNormal(ts);
